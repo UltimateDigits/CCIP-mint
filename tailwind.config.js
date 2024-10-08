1198f153b11d85e55ff986a8cd06b18f36355f98
vishalmet <vishalmet003@gmail.com>
2024-10-08 23:22:05 +0530

    default sets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customText: '#D8E4FD',
+      }
+    },
   },
   plugins: [],
 }
