66c021fd997a8b4cc1194af6e94a79018191cb2f
vishalmet <vishalmet003@gmail.com>
2024-10-09 11:58:23 +0530

    slicing the number

---
+++ src/redux/numberSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const numberSlice = createSlice({
+  name: 'number',
+  initialState: {
+    value: '',
+  },
+  reducers: {
+    setNumber: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { setNumber } = numberSlice.actions;
+export const selectNumber = (state) => state.number.value;
+export default numberSlice.reducer;
